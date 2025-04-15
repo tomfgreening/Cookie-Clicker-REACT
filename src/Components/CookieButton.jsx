0e5cdaeabd85795a000a8390f146b99b7f8f95ad
Thomas Greening <tomfgreening@gmail.com>
2025-04-15 16:53:19 +0100

    Created button that increases cookie count, using function logic.

--- src/Components/CookieButton.jsx
+++ src/Components/CookieButton.jsx
@@ -1,14 +1,15 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 export default function CookieButton() {
-    const [cookieState, setCookieState] = useState(0);
-    function increase() {
-        setCookieState(cookieState +1);
-    }
+  const [cookieState, setCookieState] = useState(0);
+  function increase() {
+    setCookieState(cookieState + 1);
+  }
 
-    return (
-        <div>
-        <p>You have this many cookies:{cookieState}, neat!</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>You have this many cookies:{cookieState}, neat!</p>
+      <button onClick={increase}>Click for another cookie!</button>
+    </div>
+  );
 }
