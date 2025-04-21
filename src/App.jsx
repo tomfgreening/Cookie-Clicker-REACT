0b1887d274d0772a63e4ce51c22a7631996f1844
Thomas Greening <tomfgreening@gmail.com>
2025-04-21 16:51:22 +0100

    Added Timer to App.jsx file, to disply timer on the page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CookieButton from "./Components/CookieButton";
+import {Timer} from "./Components/Timer";
 
 export default function App() {
     return (
         <div>
             <CookieButton/>
+            <Timer/>
         </div>
     );
 }
