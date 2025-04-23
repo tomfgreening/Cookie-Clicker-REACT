3866dd2cf8ccd6fedaeacae150cf40766e6da4b0
Thomas Greening <tomfgreening@gmail.com>
2025-04-23 19:28:37 +0100

    Lifted cookie state to app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useState } from "react";
+
 import CookieButton from "./Components/CookieButton";
 import Timer from "./Components/Timer";
 
+
 export default function App() {
+    const [currentcookieState, setCookieState] = useState(0);
     return (
         <div>
             <CookieButton/>
