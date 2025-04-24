6c15284e10faf49945e91ef13e8b8fee704cbd5d
Thomas Greening <tomfgreening@gmail.com>
2025-04-24 15:27:00 +0100

    Built increase function in app.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Timer from "./Components/Timer";
 
 export default function App() {
     const [currentcookieState, setCookieState] = useState(0);
+    function increase() {
+        setCookieState(currentcookieState +1);
+    }
     return (
         <div>
             <CookieButton/>
