a6290aba2bde9ffe0d80918f3402e2a532af9c23
Thomas Greening <tomfgreening@gmail.com>
2025-04-24 15:55:06 +0100

    Rewrote CookieButton component with cookieCount and increase props.

--- src/Components/CookieButton.jsx
+++ src/Components/CookieButton.jsx
@@ -1,14 +1,9 @@
 import { useState } from "react";
 
-export default function CookieButton() {
-  const [cookieState, setCookieState] = useState(0);
-  function increase() {
-    setCookieState(cookieState + 1);
-  }
-
+export default function CookieButton({cookieCount, increase}) {
   return (
     <div>
-      <p>You have this many cookies:{cookieState}, neat!</p>
+      <p>You have this many cookies: {cookieCount} , neat!</p>
       <button onClick={increase}>Click for another cookie!</button>
     </div>
   );
