475aa45fc6c5f3994907917e7009491f9e09d742
Thomas Greening <tomfgreening@gmail.com>
2025-05-06 11:12:16 +0100

    Used arrow function for onClick logic so that the decrease function is called when the button is clicked, rather than running as soon as the component is rendered.

--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -1,7 +1,7 @@
 export default function UpgradesTable({ cookieCost, decrease }) {
   return (
     <div>
-      <button onClick={decrease(cookieCost)}>Upgrade 1 - 50 cookies </button>
+      <button onClick={() => decrease(cookieCost)}>Upgrade 1 - 50 cookies </button>
     </div>
   );
 }
