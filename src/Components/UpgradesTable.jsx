fe221239e3273ac116f6f1f8ba076b73bda2c9c9
Thomas Greening <tomfgreening@gmail.com>
2025-05-06 11:20:26 +0100

    Added in 2 more upgrade buttons.

--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -2,6 +2,8 @@ export default function UpgradesTable({ cookieCost, decrease }) {
   return (
     <div>
       <button onClick={() => decrease(cookieCost)}>Upgrade 1 - 50 cookies </button>
+      <button onClick={() => decrease(cookieCost)}>Upgrade 2 - 100 cookies </button>
+      <button onClick={() => decrease(cookieCost)}>Upgrade 3 - 200 cookies </button>
     </div>
   );
 }
