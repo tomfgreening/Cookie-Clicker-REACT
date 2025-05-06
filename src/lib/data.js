58e7c2132c3236425cc208aac006877979fda28c
Thomas Greening <tomfgreening@gmail.com>
2025-05-06 11:52:58 +0100

    Stored cost values as numbers rather than strings.

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,22 +1,22 @@
 export const UpgradeItems = [
     {
         id: 1,
-        name:"Easy bake oven",
-        cost: "50 cookies",
+        name: "Easy bake oven",
+        cost: "50",
         effect: "One click now grants 2 cookies!"
     },
 
     {
         id: 2,
-        name:"Sugar surger",
-        cost: "100 cookies",
-        effect:"Doubles your cookies gained per second!"
+        name: "Sugar surger",
+        cost: "100",
+        effect: "Doubles your cookies gained per second!"
     },
 
     {
         id:3,
-        name:"Qauntum cookie",
-        cost:"200 cookies",
-        effect:"One click now grants 10 cookies!"
+        name: "Quantum cookie",
+        cost: "200",
+        effect: "One click now grants 10 cookies!"
     }
 ];
