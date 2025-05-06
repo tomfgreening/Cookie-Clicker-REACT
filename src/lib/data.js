4eb68da596873db06419be8701c65500d6667113
Thomas Greening <tomfgreening@gmail.com>
2025-05-06 11:50:40 +0100

    Created object array with upgrade items ready to be mapped over in the upgrade tables component.

---
+++ src/lib/data.js
@@ -0,0 +1,22 @@
+export const UpgradeItems = [
+    {
+        id: 1,
+        name:"Easy bake oven",
+        cost: "50 cookies",
+        effect: "One click now grants 2 cookies!"
+    },
+
+    {
+        id: 2,
+        name:"Sugar surger",
+        cost: "100 cookies",
+        effect:"Doubles your cookies gained per second!"
+    },
+
+    {
+        id:3,
+        name:"Qauntum cookie",
+        cost:"200 cookies",
+        effect:"One click now grants 10 cookies!"
+    }
+];
