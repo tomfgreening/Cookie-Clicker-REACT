0a07621e3dd21c569fd684b5d3a1c075100ce4c0
Thomas Greening <tomfgreening@gmail.com>
2025-05-06 11:54:50 +0100

    Changed "effect" to "description" in array for clarity.

--- src/lib/data.js
+++ src/lib/data.js
@@ -3,20 +3,21 @@ export const UpgradeItems = [
         id: 1,
         name: "Easy bake oven",
         cost: "50",
-        effect: "One click now grants 2 cookies!"
+        description: "One click now grants 2 cookies!"
+
     },
 
     {
         id: 2,
         name: "Sugar surger",
         cost: "100",
-        effect: "Doubles your cookies gained per second!"
+        description: "Doubles your cookies gained per second!"
     },
 
     {
         id:3,
         name: "Quantum cookie",
         cost: "200",
-        effect: "One click now grants 10 cookies!"
+        desription: "One click now grants 10 cookies!"
     }
 ];
