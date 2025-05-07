c7a944f53fae90b649d4e1e6ad885c620223749a
Thomas Greening <tomfgreening@gmail.com>
2025-05-07 15:12:25 +0100

    Removed quote marks from cost in UpgradeItems array, so the cost of items will be read as a number not a string.

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,21 +2,21 @@ export const UpgradeItems = [
   {
     id: 1,
     name: "Easy bake oven",
-    cost: "50",
+    cost: 50,
     description: "One click now grants 2 cookies!",
   },
 
   {
     id: 2,
     name: "Sugar surger",
-    cost: "100",
+    cost: 100,
     description: "Doubles your cookies gained per second!",
   },
 
   {
     id: 3,
     name: "Quantum cookie",
-    cost: "200",
+    cost: 200,
     description: "One click now grants 10 cookies!",
   },
 ];
