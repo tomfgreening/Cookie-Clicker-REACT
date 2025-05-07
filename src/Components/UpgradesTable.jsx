54ce9f6213bf624175ec6c6d00edb74ff02bc28a
Thomas Greening <tomfgreening@gmail.com>
2025-05-07 15:00:55 +0100

    For each UpgradesItem looped over, the fucntion will create a button.

--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -3,14 +3,11 @@ import { UpgradeItems } from "../lib/data";
 export default function UpgradesTable({ decrease }) {
   return (
     <div>
-    {UpgradeItems.map((item) => (
-      <button
-      key={item.id}
-      onClick={() => decrease(Number(item.cost))}
-      >
-        {item.name} - {item.cost} cookies
+    {UpgradeItems.map((item) => ()
+      <button>
+      
       </button>
-    ))}
+    }
     </div>
   );
 }
