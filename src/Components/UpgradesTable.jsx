e1d33d27ed81fc84793983eeccbcf525cf8799bc
Thomas Greening <tomfgreening@gmail.com>
2025-05-07 14:43:33 +0100

    Removed hardcoded buttons from upgradestable component. to be replaced with .map function.

--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -1,9 +1,9 @@
+import { UpgradeItems } from "../lib/data";
+
 export default function UpgradesTable({ cookieCost, decrease }) {
   return (
     <div>
-      <button onClick={() => decrease(cookieCost)}>Upgrade 1 - 50 cookies </button>
-      <button onClick={() => decrease(cookieCost)}>Upgrade 2 - 100 cookies </button>
-      <button onClick={() => decrease(cookieCost)}>Upgrade 3 - 200 cookies </button>
+    
     </div>
   );
 }
