15b058e0046698ec65850c4cbf6102e8bce86f37
Thomas Greening <tomfgreening@gmail.com>
2025-05-07 14:59:45 +0100

    Writing function logic. Function will loop through each item from the Upgradeitems array.

--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -1,9 +1,16 @@
 import { UpgradeItems } from "../lib/data";
 
-export default function UpgradesTable({ cookieCost, decrease }) {
+export default function UpgradesTable({ decrease }) {
   return (
     <div>
-    
+    {UpgradeItems.map((item) => (
+      <button
+      key={item.id}
+      onClick={() => decrease(Number(item.cost))}
+      >
+        {item.name} - {item.cost} cookies
+      </button>
+    ))}
     </div>
   );
 }
