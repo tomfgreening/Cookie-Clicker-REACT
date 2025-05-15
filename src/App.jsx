711ab69c1fb4a8bdcbe2de063eb389fb3b7b8fb2
Thomas Greening <tomfgreening@gmail.com>
2025-05-15 12:22:22 +0100

    TROUBLESHOOTING. Passed count as props to upgrade table and put it in the if statement in handleUpgrades function. Has had an effect on the cookies per second count but no the inteded effect.

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ export default function App() {
         cookiesPerSecond={cookiesPerSecond}
         setCookiesPerSecond={setCookiesPerSecond}
         totalCookieCount={totalCookieCount}
+        count={count}
       />
     </div>
   );
--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -1,13 +1,13 @@
 import { UpgradeItems } from "../lib/data";
 
-export default function UpgradesTable({decrease, cookiesPerSecond,setCookiesPerSecond, totalCookieCount,}) {
+export default function UpgradesTable({decrease, cookiesPerSecond,setCookiesPerSecond, totalCookieCount, count}) {
   function handleUpgrades(item) {
     if (totalCookieCount < item.cost) {
       console.log("you do not have enough cookies to purchase this upgrade.");
     } else {
       decrease(item.cost);
      if ((item.id == 2)) {
-      setCookiesPerSecond(cookiesPerSecond * 2);
+      setCookiesPerSecond(count * 2);
     }}
    
   }
