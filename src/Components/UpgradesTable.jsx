aec835cdf131cf96dd9b07e78513a81abeb56c82
Thomas Greening <tomfgreening@gmail.com>
2025-05-19 15:56:04 +0100

    TROUBLESHOOTING. IF statement logic causing upgrade purchase  to display NaN in total cookies count.

--- src/Components/UpgradesTable.jsx
+++ src/Components/UpgradesTable.jsx
@@ -1,19 +1,25 @@
 import { UpgradeItems } from "../lib/data";
 
-export default function UpgradesTable({increase, decrease, cookiesPerSecond,setCookiesPerSecond, setCookieCount, totalCookieCount}) {
+export default function UpgradesTable({
+  increase,
+  decrease,
+  cookiesPerSecond,
+  setCookiesPerSecond,
+  setCookieCount,
+  totalCookieCount,
+}) {
   function handleUpgrades(item) {
     if (totalCookieCount < item.cost) {
       console.log("you do not have enough cookies to purchase this upgrade.");
     } else {
       decrease(item.cost);
-     if ((item.id == 2)) {
-      setCookiesPerSecond(cookiesPerSecond * 2);
+      if (item.id == 2) {
+        setCookiesPerSecond(cookiesPerSecond * 2);
+      }
+      if (item.id == 1) {
+        setCookieCount(increase * 2);
+      }
     }
-    if ((item.id == 1)) {
-      setCookieCount(increase * 2);
-    }
-    }
-   
   }
 
   return (
