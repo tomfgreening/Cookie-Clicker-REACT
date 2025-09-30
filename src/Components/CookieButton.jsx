999d4161e3b50fd58590dc7298e6d1e9a23bd8ed
Thomas Greening <tomfgreening@gmail.com>
2025-09-30 16:26:14 +0100

    Centered elements within the grid.

--- src/Components/CookieButton.jsx
+++ src/Components/CookieButton.jsx
@@ -1,7 +1,5 @@
 export default function CookieButton({ increase }) {
   return (
-   <div class="buttonContainer">
-      <img src="/clickmecookie.png" alt="Computer generated image of a cookie" onClick={increase}/>
-      </div>   
+      <img className="CookieButton" src="/clickmecookie.png" alt="Computer generated image of a cookie" onClick={increase}/>
    );
 }
