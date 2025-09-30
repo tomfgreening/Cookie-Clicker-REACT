a1ec756feaa9b96127a94c9f776c4ca7290f36df
Thomas Greening <tomfgreening@gmail.com>
2025-09-30 11:39:21 +0100

    Adjusted upgrade name text for better readability.

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,21 +1,21 @@
 export const UpgradeItems = [
   {
     id: 1,
-    name: "Easy bake oven",
+    name: "EASY BAKE OVEN",
     cost: 50,
     description: "One click now grants 2 cookies!",
   },
 
   {
     id: 2,
-    name: "Sugar surger",
+    name: "SUGAR SURGER",
     cost: 100,
     description: "Doubles your cookies gained per second!",
   },
 
   {
     id: 3,
-    name: "Quantum cookie",
+    name: "QUANTAM COOKIE",
     cost: 200,
     description: "One click now grants 10 cookies!",
   },
