6c5fbcdb78f9d482e6690c586cdb4eff15f731e0
Tom <tomfgreening@gmail.com>
2026-06-10 17:49:17 +0100

    Changed colour scheme to better fit what a user would expect to see from a cookie clicker game.

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,21 +1,21 @@
 export const UpgradeItems = [
   {
     id: 1,
-    name: "EASY-BAKE OVEN",
+    name: "Easy-bake Oven",
     cost: 50,
     description: "One click now grants 2 cookies!",
   },
 
   {
     id: 2,
-    name: "SUGAR SURGE",
+    name: "Sugar Surger",
     cost: 100,
     description: "Doubles your cookies gained per second!",
   },
 
   {
     id: 3,
-    name: "QUANTUM COOKIE",
+    name: "Quantum Cookie",
     cost: 200,
     description: "One click now grants 10 cookies!",
   },
