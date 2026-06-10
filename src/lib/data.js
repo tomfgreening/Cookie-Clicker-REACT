b39e5aaf6428b35244980824f3b4705f6c25484a
Tom <tomfgreening@gmail.com>
2026-06-10 16:59:24 +0100

    Wrapped upgrade elements in H2 and P tags, to target them individually for styling.

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,21 +1,21 @@
 export const UpgradeItems = [
   {
     id: 1,
-    name: "EASY BAKE OVEN",
+    name: "EASY-BAKE OVEN",
     cost: 50,
     description: "One click now grants 2 cookies!",
   },
 
   {
     id: 2,
-    name: "SUGAR SURGER",
+    name: "SUGAR SURGE",
     cost: 100,
     description: "Doubles your cookies gained per second!",
   },
 
   {
     id: 3,
-    name: "QUANTAM COOKIE",
+    name: "QUANTUM COOKIE",
     cost: 200,
     description: "One click now grants 10 cookies!",
   },
